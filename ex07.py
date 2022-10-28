171b7871e4559aa49394a0a13f35488c60335d06
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-10-27 22:12:14 -0300

    Estrutura de repetição adicionada

--- ex07.py
+++ ex07.py
@@ -5,29 +5,34 @@ print('1 - PAGAMENTO À VISTA EM DINHIERO, RECEBE 15% DE DESCONTO')
 print('2 - PAGAMENTO À VISTA NO CARTÃO DE CRÉDITO, RECEBE 10% DE DESCONTO')
 print('3 - PAGAMENTO EM DUAS VEZES, PREÇO NORMAL DO PRODUTO')
 print('4 - PAGAMENTO EM MAIS DE DUAS VEZES, PREÇO NORMAL DO PRODUTO,MAIS JUROS DE 10%')
-
-try:
-    valor_produto = float(input('Digite o valor do produto: '))
-except ValueError:
-    print('VALOR INVÁLIDO')
-try:
-    forma_pagamento = int(input('Digite a forma de pagamento conforme opções acima: '))
-except ValueError:
-    print('VALOR INVÁLIDO')
-if forma_pagamento == 1:
-    valor_final = valor_produto - (valor_produto * 0.15)
-    print(f'Valor Final para pagamento à vista no dinheiro R$ {valor_final:.2f}.')
+print('5 - SAIR')
+forma_pagamento = 0
+while True:
+    try:
+        valor_produto = float(input('Digite o valor do produto: '))
+    except ValueError:
+        print('VALOR INVÁLIDO')
+    try:
+        forma_pagamento = int(input('Digite a forma de pagamento conforme opções acima: '))
+    except ValueError:
+        print('VALOR INVÁLIDO')
+    if forma_pagamento == 1:
+        valor_final = valor_produto - (valor_produto * 0.15)
+        print(f'Valor Final para pagamento à vista no dinheiro R$ {valor_final:.2f}.')
     
-elif forma_pagamento == 2:
-    valor_final = valor_produto - (valor_produto * 0.10)
-    print(f'Valor Final para pagamento à vista no cartão de crédito R$ {valor_final:.2f}.')
+    elif forma_pagamento == 2:
+        valor_final = valor_produto - (valor_produto * 0.10)
+        print(f'Valor Final para pagamento à vista no cartão de crédito R$ {valor_final:.2f}.')
     
-elif forma_pagamento == 3:
-    valor_final = valor_produto
-    print(f'Valor Final para pagamento em duas vezes R$ {valor_final:.2f}.')
+    elif forma_pagamento == 3:
+        valor_final = valor_produto
+        print(f'Valor Final para pagamento em duas vezes R$ {valor_final:.2f}.')
+        
+    elif forma_pagamento == 4:
+        valor_final = valor_produto + (valor_produto * 0.10)
+        print(f'Valor Final para pagamento em mais de duas vezes R$ {valor_final:.2f}.')
     
-elif forma_pagamento == 4:
-    valor_final = valor_produto + (valor_produto * 0.10)
-    print(f'Valor Final para pagamento em mais de duas vezes R$ {valor_final:.2f}.')
+    elif forma_pagamento == 5:
+        break
     
 print('OBRIGADO')
