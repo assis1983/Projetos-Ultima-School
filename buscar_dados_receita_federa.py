63f426b6e70b7e9667cdfcf146603fe0474e0451
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-12-21 12:25:34 -0300

    Arquivos Adicionados

---
+++ buscar_dados_receita_federa.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+from openpyxl import workbook
+
+html = urlopen("https://www.gov.br/receitafederal/pt-br/assuntos/agenda-tributaria/agenda-tributaria-2022/dezembro/dia-05-12-2022")
+bs = BeautifulSoup(html, 'html.parser')
+linhas = bs.find_all('tr', {'class':'even'})
+
+codigo, descricao, periodo = [], [], []
+for i in linhas:
+    children = i.findChildren("td")
+    codigo.append(children[0].text)
+    descricao.append(children[1].text)
+    periodo.append(children[2].text)
+
+df = pd.DataFrame({'CÓDIGO': codigo, 'DESCRIÇÃO': descricao, 'PERÍODO': periodo})
+df.head()
+print(df)
+
+df.to_excel('dados.xlsx')
